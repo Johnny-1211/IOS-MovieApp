ac0f913628ae9b02d5543e176fbf8a8cb9e418da
Johnny <tamtingyuen@gmail.com>
2024-05-17 23:51:42 -0400

    fix : logout button disappear in account view

--- Movie/Screens/MoviesTabView.swift
+++ Movie/Screens/MoviesTabView.swift
@@ -4,23 +4,67 @@ struct MoviesTabView: View {
     @EnvironmentObject var fireAuthHelper : FireAuthHelper
     @EnvironmentObject var fireDBHelper : FireDBHelper
     @Binding var rootScreen : RootView
-    
-    
-    var body: some View {
-        ZStack{
-            TabView {
-                HomeView()
-                    .tabItem { Label("Home", systemImage: "house").padding(.top, 5) }
-                    .environmentObject(fireDBHelper)
+    @State private var selectedTab = 0
+    @State private var isLogOut: Bool = false
 
-                AccountView(rootScreen: $rootScreen)
-                    .tabItem { Label("Account", systemImage: "person").padding(.top, 5) }
-                    .environmentObject(fireDBHelper)
-                    .environmentObject(fireAuthHelper)
-                OrderListView()
-                    .tabItem { Label("Order", systemImage: "bag").padding(.top, 5) }
+        
+    var body: some View {
+        NavigationStack{
+            ZStack{
+                Color.clear
+                    .ignoresSafeArea()
+                TabView(selection: $selectedTab){
+                    HomeView()
+                        .tabItem { Label("Home", systemImage: "house").padding(.top, 5) }
+                        .tag(0)
+                    
+                    AccountView(rootScreen: $rootScreen)
+                        .tabItem { Label("Account", systemImage: "person").padding(.top, 5) }
+                        .tag(1)
+                    
+                    OrderListView()
+                        .tabItem { Label("Order", systemImage: "bag").padding(.top, 5) }
+                        .tag(2)
+                }
+                .toolbar {
+                    if selectedTab == 1 {
+                        ToolbarItem(placement: .topBarTrailing) {
+                            Button{
+                                isLogOut = true
+                            }label:{
+                                HStack{
+                                    Image(systemName: "figure.walk.arrival")
+                                    Text("Log Out")
+                                }
+                                .foregroundStyle(.orange)
+                                .font(.body.bold())
+                                .padding(10)
+                                .clipShape(
+                                    RoundedRectangle(cornerRadius: 12)
+                                )
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 12)
+                                        .stroke(.orange, lineWidth: 2)
+                                )
+                            }
+                            .alert(isPresented: $isLogOut){
+                                Alert(title: Text("Log Out"),
+                                      message: Text("Do you want to logout?"),
+                                      primaryButton: .default(Text("Log Out")) {
+                                    fireAuthHelper.signOut()
+                                    rootScreen = .Login
+                                },
+                                      secondaryButton: .cancel()
+                                )
+                            }
+                        }
+                    }
+                }
+                .environmentObject(fireDBHelper)
+                .environmentObject(fireAuthHelper)
             }
-            
         }
     }
 }
+
+
